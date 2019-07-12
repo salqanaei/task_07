8ecbab3ba798264400ca612bbfb42308a971a520
Sulaiman AlQanaei <salqanaei@Sulaimans-MacBook-Air-2.local>
2019-07-12 11:40:55 +0300

    Update & Delete

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -5,3 +5,8 @@ class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = '__all__'
+
+class UpdatRestaurant(forms.ModelForm):
+	class Meta:
+		model = Restaurant
+		fields = ['description', 'opening_time', 'closing_time']
